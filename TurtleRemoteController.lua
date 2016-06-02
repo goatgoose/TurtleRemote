b7d78ab99704d8d2cdf6aec58ecfafaf2d195faf
Sam Clark <sam.clark1142@gmail.com>
2016-06-02 17:02:42 -0400

    initial

---
+++ TurtleRemoteController.lua
@@ -0,0 +1,26 @@
+
+function drawHorizontalMap(x, y, xLim, yLim)
+
+end
+
+function drawVerticalMap(x, y, xLim, yLim)
+
+end
+
+function drawPlayerList(x, y, xLim, yLim)
+
+end
+
+function updateInterface()
+
+end
+
+function rednetListen()
+
+end
+
+function getInput()
+
+end
+
+parallel.waitForAny(rednetListen, getInput)
